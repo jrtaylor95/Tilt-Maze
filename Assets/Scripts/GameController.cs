9f77736b519e14086bdeb5a143f727b3367a8bc5
jrtaylor <jrtaylor95@gmail.com>
2017-02-08 22:23:19 -0500

    Initial Commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.R)) {
+			UnityEngine.SceneManagement.SceneManager.LoadScene ("Maze_0");
+		} else if (Input.GetKeyUp (KeyCode.Q)) {
+			Application.Quit ();
+		}
+	}
+}
---
+++ Assets/Scripts/ballController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballController : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.name == "Goal") {
+//			rb.useGravity = false;
+			rb.AddForce (Vector3.up * 1000f);
+			Destroy (gameObject, .5f);
+		}
+	}
+}
